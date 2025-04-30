95d117a6c2d9940a3f1cb04b262ac9917d49839d
Sebastian Jeworutzki <sebastian.jeworutzki@ruhr-uni-bochum.de>
2025-04-30 11:59:04 +0200

    Fix CRAN-check namespace issues with markdown test

--- tests/testthat/test-markdown.R
+++ tests/testthat/test-markdown.R
@@ -1,6 +1,6 @@
 test_that("R Markdown documents can be rendered", {
   skip_on_cran()
-  skip_if_not_installed("rmarkdown")
+  skip_if_not_installed(c("rmarkdown", "tmap"))
   rmarkdown::render("test.Rmd", quiet = TRUE)
   expect_true(file.exists("test.html"))
   unlink("test.html")
